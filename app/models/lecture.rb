0ca8f86b74985f5b667c01083cfde993b58d3efb
Yoji Ito <yoji.ito@gmail.com>
2016-03-08 09:54:01 +0900

    dateの認証など

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,5 +1,6 @@
 class Lecture < ActiveRecord::Base
   belongs_to :user
+  validates :date, presence: true
   validates :title, presence: true
   validates :place, presence: true
   validates :detail, presence: true
--- app/models/scraping_arigato.rb
+++ app/models/scraping_arigato.rb
@@ -5,16 +5,16 @@
      agent = Mechanize.new
      links = []
 
-     next_url = "http://www.rikkyo.ac.jp/events/2015/09/"
+     next_url = "http://www.39asset.co.jp/html/seminar/index/htm"
 
     while true do
      page = agent.get(next_url)
-     elements = page.search('.right td span a')
+     elements = page.search('.seminer_entry_list li a')
      elements.each do |ele|
      links << ele[:href]
    end
 
-    next_link = page.at('.right ul .next a')
+    next_link = page.at('.fm_entry_list_next a')
     next_url = next_link.get_attribute('href')
     break unless next_url
 
@@ -28,10 +28,10 @@ end
      def self.get_lecture(link)
         agent = Mechanize.new
         page = agent.get(link)
-        title = page.at('.right h3 span').inner_text
-        date = page.at('.bgc_000000093').inner_text if page.at('.bgc_000000093')
-        place = page.at('.bgc_000000093 a').inner_text if page.at('.bgc_000000093 a')
-        detail = page.at('.right p').inner_text if page.at('.right p')
+        title = page.at('.contents h2').inner_text
+        date = page.at('.contents .seminer_entry_dat').inner_text if page.at('.contents .seminer_entry_dat')
+        place = page.at('.contents .seminer_entry_loca').inner_text if page.at('.contents .seminer_entry_loca')
+        detail = page.at('.contents .seminaer_entry_body').inner_text if page.at('.contents .seminaer_entry_body')
 
         lecture = Lecture.where(title: title, date: date, place: place, detail: detail).first_or_initialize
         lecture.save
